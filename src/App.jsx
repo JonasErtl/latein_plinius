fa258338c3659320d625461989b2b74bf12742f3
JonasErtl <nilshofmeier@gmail.com>
2023-01-13 18:47:09 +0100

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,24 @@ import Impressum from './components/Impressum'
 import Quiz from './components/Quiz'
 import Biographie from './components/Biographie'
 import Zusammenfassung from './components/Zusammenfassung'
+import NoPage from "./components/NoPage"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-    </div>
+    
+      <div className="App">
+        <Navbar/>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Quiz/>}/>
+            <Route path="/bio" element={<Biographie/>}/>
+            <Route path="/impressum" element={<Impressum/>}/>
+            <Route path="/overwiev" element={<Zusammenfassung/>}/>
+            <Route path="/*" element={<NoPage/>}/>
+          </Routes>
+        </BrowserRouter>
+      </div>
   )
 }
 
---
+++ src/components/NoPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoPage = () => {
+  return (
+    <div>Error 404. This page does not exist.</div>
+  )
+}
+
+export default NoPage
