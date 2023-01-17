406835f922866ddef6c708cff3fb973c01e61ceb
JonasErtl <nilshofmeier@gmail.com>
2023-01-17 18:26:04 +0100

    fix

--- src/components/Zusammenfassung.jsx
+++ src/components/Zusammenfassung.jsx
@@ -5,7 +5,7 @@ const Zusammenfassung = () => {
   return (
     <div>
       <div className="bg-gradient-to-r from-white to-white h-screen pt-10">
-    <div className='bg-gradient-to-tr from-gray-50 to-werid flex justify-center mx-14 h-1/3 rounded shadow-xl text-gray-600 text-center text-xl h-auto'>
+    <div className='bg-gradient-to-tr from-gray-50 to-werid flex justify-center mx-14 h-auto rounded shadow-xl text-gray-600 text-center text-xl'>
       <h1 className='text-center'>
         Impressum 
       </h1>
