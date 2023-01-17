1e971081f96d227c9949421add05add7c47e5962
JonasErtl <nilshofmeier@gmail.com>
2023-01-17 18:30:54 +0100

    mobile view update

--- src/components/Biographie.jsx
+++ src/components/Biographie.jsx
@@ -4,7 +4,7 @@ import Animation from './Animation'
 const Biographie = () => {
   return (
     <div>
-      <div className="bg-gradient-to-r from-white to-white h-screen pt-10">
+      <div className="bg-gradient-to-r from-white to-white h-screen pt-10 pb-10">
     <div className='bg-gradient-to-tr from-gray-50 to-werid flex justify-center mx-14 h-auto rounded shadow-xl text-gray-600 text-center text-xl'>
       <h1 className='text-center'>
         Impressum 
--- src/components/Impressum.jsx
+++ src/components/Impressum.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Impressum = () => {
   return (
-    <div className="bg-gradient-to-r from-white to-white h-screen pt-10">
+    <div className="bg-gradient-to-r from-white to-white h-screen pt-10 pb-10">
     <div className='bg-gradient-to-tr from-gray-50 to-werid flex justify-center mx-14 h-auto rounded shadow-xl text-gray-600 text-center text-xl'>
       <h1 className='text-center'>
         Impressum 
